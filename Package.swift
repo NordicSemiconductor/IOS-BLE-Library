9dccdf86dac12854884139d29290046d27ad4bc4
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2022-08-23 12:23:40 +0100

    iOS-BLE-Library now sets iOS-Common-Libraries as a dependency, plus AdvertisementData struct
    This was tricky to set up.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "iOS-BLE-Library",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,14 +17,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/NordicPlayground/IOS-Common-Libraries", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iOS-BLE-Library",
-            dependencies: []),
+            dependencies: [.product(name: "iOSCommonLibraries", package: "iOS-Common-Libraries")]),
         .testTarget(
             name: "iOS-BLE-LibraryTests",
             dependencies: ["iOS-BLE-Library"]),
