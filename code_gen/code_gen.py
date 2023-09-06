4e84dd8a60839f5d3391f253ec3dd510e4a6d6da
Nick Kibysh <nick.kibysh@gmail.com>
2023-09-06 17:36:01 +0200

    fixed code_gen script to copy Alias file from any destination

--- code_gen/code_gen.py
+++ code_gen/code_gen.py
@@ -11,9 +11,14 @@ if __name__ == '__main__':
     destination = os.path.join(sources_dir, 'iOS-BLE-Library-Mock')
     cf.copy_all_files(source, destination)
 
-    additional_files_dir = os.path.join(os.getcwd(), 'additional_files')
+    script_path = os.path.join(os.getcwd(), sys.argv[0])
+    script_path = os.path.dirname(script_path)
+    additional_files_dir = os.path.join(script_path, 'additional_files')
+
     additional_files_dir_destination = os.path.join(destination, 'Additional Files')
     cf.copy_all_files(additional_files_dir, destination)
+
+    print('arg0: ' + sys.argv[0])
 
     print('replacing code...')
     # modify all .swift files in the destination directory and its subdirectories
