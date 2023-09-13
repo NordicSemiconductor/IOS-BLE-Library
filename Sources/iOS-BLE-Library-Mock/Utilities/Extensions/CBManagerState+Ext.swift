9a9cfe4a51646b685c433776cfdc2cc532d6fa22
NickKibish <NickKibish@users.noreply.github.com>
2023-09-13 09:26:33 +0000

    Copied files from native CoreBluetooth version to CoreBluetoothMock

---
+++ Sources/iOS-BLE-Library-Mock/Utilities/Extensions/CBManagerState+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//
+//
+//  Created by Nick Kibysh on 19/04/2023.
+//
+
+import CoreBluetoothMock
+import Foundation
+
+extension CBManagerState {
+	var ready: Bool? {
+		switch self {
+		case .poweredOn:
+			return true
+		case .unknown, .resetting:
+			return nil
+		case .poweredOff, .unauthorized, .unsupported:
+			return false
+		}
+	}
+}
