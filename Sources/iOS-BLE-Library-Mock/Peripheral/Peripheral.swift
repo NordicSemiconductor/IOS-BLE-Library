0f899bcf9e47712417cf792a1f9113f57136766a
NickKibish <NickKibish@users.noreply.github.com>
2023-10-06 09:55:04 +0000

    Copied files from native CoreBluetooth version to CoreBluetoothMock

--- Sources/iOS-BLE-Library-Mock/Peripheral/Peripheral.swift
+++ Sources/iOS-BLE-Library-Mock/Peripheral/Peripheral.swift
@@ -7,7 +7,6 @@
 
 import Combine
 import CoreBluetooth
-
 import CoreBluetoothMock
 import Foundation
 
@@ -43,28 +42,31 @@ private class NativeObserver: Observer {
 }
 
 private class MockObserver: Observer {
-     @objc private var peripheral: CBMPeripheralMock
+	@objc private var peripheral: CBMPeripheralMock
 
-     private weak var publisher: CurrentValueSubject<CBPeripheralState, Never>!
-     private var observation: NSKeyValueObservation?
+	private weak var publisher: CurrentValueSubject<CBPeripheralState, Never>!
+	private var observation: NSKeyValueObservation?
 
-     init(peripheral: CBMPeripheralMock, publisher: CurrentValueSubject<CBPeripheralState, Never>) {
-         self.peripheral = peripheral
-         self.publisher = publisher
-         super.init()
-     }
+	init(
+		peripheral: CBMPeripheralMock,
+		publisher: CurrentValueSubject<CBPeripheralState, Never>
+	) {
+		self.peripheral = peripheral
+		self.publisher = publisher
+		super.init()
+	}
 
-     override func setup() {
-         observation = peripheral.observe(\.state, options: [.new]) { [weak self] _, change in
-             #warning("queue can be not only main")
-             DispatchQueue.main.async {
-                 guard let self else { return }
-                 self.publisher.send(self.peripheral.state)
-             }
-         }
-     }
- }
-
+	override func setup() {
+		observation = peripheral.observe(\.state, options: [.new]) {
+			[weak self] _, change in
+			#warning("queue can be not only main")
+			DispatchQueue.main.async {
+				guard let self else { return }
+				self.publisher.send(self.peripheral.state)
+			}
+		}
+	}
+}
 
 public class Peripheral {
 	/// I'm Errr from Omicron Persei 8
@@ -94,22 +96,22 @@ public class Peripheral {
 
 	// TODO: Why don't we use default delegate?
 	/// Initializes a Peripheral instance.
-    ///
-    /// - Parameters:
-    ///   - peripheral: The CBPeripheral to manage.
-    ///   - delegate: The delegate for handling peripheral events.
+	///
+	/// - Parameters:
+	///   - peripheral: The CBPeripheral to manage.
+	///   - delegate: The delegate for handling peripheral events.
 	public init(peripheral: CBPeripheral, delegate: ReactivePeripheralDelegate) {
 		self.peripheral = peripheral
 		self.peripheralDelegate = delegate
 		peripheral.delegate = delegate
 
-if let p = peripheral as? CBMPeripheralNative {
-                     observer = NativeObserver(peripheral: p.peripheral, publisher: stateSubject)
-                     observer.setup()
-                 } else if let p = peripheral as? CBMPeripheralMock {
-                     observer = MockObserver(peripheral: p, publisher: stateSubject)
-                     observer.setup()
-                 }
+		if let p = peripheral as? CBMPeripheralNative {
+			observer = NativeObserver(peripheral: p.peripheral, publisher: stateSubject)
+			observer.setup()
+		} else if let p = peripheral as? CBMPeripheralMock {
+			observer = MockObserver(peripheral: p, publisher: stateSubject)
+			observer.setup()
+		}
 	}
 }
 
@@ -123,9 +125,9 @@ extension Peripheral {
 
 extension Peripheral {
 	/// Discover services for the peripheral.
-    ///
-    /// - Parameter serviceUUIDs: An optional array of service UUIDs to filter the discovery results. If nil, all services will be discovered.
-    /// - Returns: A publisher emitting discovered services or an error.
+	///
+	/// - Parameter serviceUUIDs: An optional array of service UUIDs to filter the discovery results. If nil, all services will be discovered.
+	/// - Returns: A publisher emitting discovered services or an error.
 	public func discoverServices(serviceUUIDs: [CBUUID]?)
 		-> AnyPublisher<[CBService], Error>
 	{
@@ -136,22 +138,22 @@ extension Peripheral {
 				} else {
 					return result.0
 				}
-            }
-            .first()
+			}
+			.first()
 
 		return allServices.bluetooth {
 			self.peripheral.discoverServices(serviceUUIDs)
 		}
-        .autoconnect()
-        .eraseToAnyPublisher()
+		.autoconnect()
+		.eraseToAnyPublisher()
 	}
 
 	/// Discover characteristics for a given service.
-    ///
-    /// - Parameters:
-    ///   - characteristicUUIDs: An optional array of characteristic UUIDs to filter the discovery results. If nil, all characteristics will be discovered.
-    ///   - service: The service for which to discover characteristics.
-    /// - Returns: A publisher emitting discovered characteristics or an error.
+	///
+	/// - Parameters:
+	///   - characteristicUUIDs: An optional array of characteristic UUIDs to filter the discovery results. If nil, all characteristics will be discovered.
+	///   - service: The service for which to discover characteristics.
+	/// - Returns: A publisher emitting discovered characteristics or an error.
 	public func discoverCharacteristics(
 		_ characteristicUUIDs: [CBUUID]?, for service: CBService
 	) -> AnyPublisher<[CBCharacteristic], Error> {
@@ -166,19 +168,19 @@ extension Peripheral {
 					return result.1
 				}
 			}
-            .first()
+			.first()
 
 		return allCharacteristics.bluetooth {
 			self.peripheral.discoverCharacteristics(characteristicUUIDs, for: service)
 		}
-        .autoconnect()
-        .eraseToAnyPublisher()
+		.autoconnect()
+		.eraseToAnyPublisher()
 	}
 
 	/// Discover descriptors for a given characteristic.
-    ///
-    /// - Parameter characteristic: The characteristic for which to discover descriptors.
-    /// - Returns: A publisher emitting discovered descriptors or an error.
+	///
+	/// - Parameter characteristic: The characteristic for which to discover descriptors.
+	/// - Returns: A publisher emitting discovered descriptors or an error.
 	public func discoverDescriptors(for characteristic: CBCharacteristic)
 		-> AnyPublisher<[CBDescriptor], Error>
 	{
@@ -193,23 +195,23 @@ extension Peripheral {
 					return result.1
 				}
 			}
-            .first()
+			.first()
 			.bluetooth {
 				self.peripheral.discoverDescriptors(for: characteristic)
 			}
-            .autoconnect()
-            .eraseToAnyPublisher()
+			.autoconnect()
+			.eraseToAnyPublisher()
 	}
 }
 
 // MARK: - Writing Characteristic and Descriptor Values
 extension Peripheral {
 	/// Write data to a characteristic and wait for a response.
-    ///
-    /// - Parameters:
-    ///   - data: The data to write.
-    ///   - characteristic: The characteristic to write to.
-    /// - Returns: A publisher indicating success or an error.
+	///
+	/// - Parameters:
+	///   - data: The data to write.
+	///   - characteristic: The characteristic to write to.
+	/// - Returns: A publisher indicating success or an error.
 	public func writeValueWithResponse(_ data: Data, for characteristic: CBCharacteristic)
 		-> AnyPublisher<Void, Error>
 	{
@@ -226,24 +228,24 @@ extension Peripheral {
 				self.peripheral.writeValue(
 					data, for: characteristic, type: .withResponse)
 			}
-            .autoconnect()
-            .eraseToAnyPublisher()
+			.autoconnect()
+			.eraseToAnyPublisher()
 	}
 
 	/// Write data to a characteristic without waiting for a response.
-    ///
-    /// - Parameters:
-    ///   - data: The data to write.
-    ///   - characteristic: The characteristic to write to.
+	///
+	/// - Parameters:
+	///   - data: The data to write.
+	///   - characteristic: The characteristic to write to.
 	public func writeValueWithoutResponse(_ data: Data, for characteristic: CBCharacteristic) {
 		peripheral.writeValue(data, for: characteristic, type: .withoutResponse)
 	}
 
 	/// Write data to a descriptor.
-    ///
-    /// - Parameters:
-    ///   - data: The data to write.
-    ///   - descriptor: The descriptor to write to.
+	///
+	/// - Parameters:
+	///   - data: The data to write.
+	///   - descriptor: The descriptor to write to.
 	public func writeValue(_ data: Data, for descriptor: CBDescriptor) {
 		fatalError()
 	}
@@ -252,17 +254,17 @@ extension Peripheral {
 // MARK: - Reading Characteristic and Descriptor Values
 extension Peripheral {
 	/// Read the value of a characteristic.
-    ///
-    /// - Parameter characteristic: The characteristic to read from.
-    /// - Returns: A future emitting the read data or an error.
+	///
+	/// - Parameter characteristic: The characteristic to read from.
+	/// - Returns: A future emitting the read data or an error.
 	public func readValue(for characteristic: CBCharacteristic) -> Future<Data?, Error> {
 		return reader.readValue(from: characteristic)
 	}
 
 	/// Listen for updates to the value of a characteristic.
-    ///
-    /// - Parameter characteristic: The characteristic to monitor for updates.
-    /// - Returns: A publisher emitting characteristic values or an error.
+	///
+	/// - Parameter characteristic: The characteristic to monitor for updates.
+	/// - Returns: A publisher emitting characteristic values or an error.
 	public func listenValues(for characteristic: CBCharacteristic) -> AnyPublisher<Data, Error>
 	{
 		return peripheralDelegate.updatedCharacteristicValuesSubject
@@ -278,9 +280,9 @@ extension Peripheral {
 	}
 
 	/// Read the value of a descriptor.
-    ///
-    /// - Parameter descriptor: The descriptor to read from.
-    /// - Returns: A future emitting the read data or an error.
+	///
+	/// - Parameter descriptor: The descriptor to read from.
+	/// - Returns: A future emitting the read data or an error.
 	public func readValue(for descriptor: CBDescriptor) -> Future<Data, Error> {
 		fatalError()
 	}
@@ -289,18 +291,18 @@ extension Peripheral {
 // MARK: - Setting Notifications for a Characteristic’s Value
 extension Peripheral {
 	/// Set notification state for a characteristic.
-    ///
-    /// - Parameters:
-    ///   - isEnabled: Whether notifications should be enabled or disabled.
-    ///   - characteristic: The characteristic for which to set the notification state.
-    /// - Returns: A publisher indicating success or an error.
+	///
+	/// - Parameters:
+	///   - isEnabled: Whether notifications should be enabled or disabled.
+	///   - characteristic: The characteristic for which to set the notification state.
+	/// - Returns: A publisher indicating success or an error.
 	public func setNotifyValue(_ isEnabled: Bool, for characteristic: CBCharacteristic)
 		-> AnyPublisher<Bool, Error>
 	{
 		if characteristic.isNotifying == isEnabled {
 			return Just(isEnabled)
 				.setFailureType(to: Error.self)
-                .eraseToAnyPublisher()
+				.eraseToAnyPublisher()
 		}
 
 		return peripheralDelegate.notificationStateSubject
@@ -314,7 +316,7 @@ extension Peripheral {
 			.bluetooth {
 				self.peripheral.setNotifyValue(isEnabled, for: characteristic)
 			}
-            .autoconnect()
-            .eraseToAnyPublisher()
+			.autoconnect()
+			.eraseToAnyPublisher()
 	}
 }
