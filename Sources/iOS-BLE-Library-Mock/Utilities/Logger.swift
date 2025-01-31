1e23ed2bf0a9a270112ad085fcdfae9e4109e9bf
Nick Kibysh <nick.kibysh@wtw.no>
2025-01-31 09:40:32 +0100

    fix(CBManagerState): Added check for RetroactiveAttribute to avoid crash on older Xcode versions
    @retroactive is a Swift 6 feature, so it leads to crash on older Xcodes

--- Sources/iOS-BLE-Library-Mock/Utilities/Logger.swift
+++ Sources/iOS-BLE-Library-Mock/Utilities/Logger.swift
@@ -10,76 +10,76 @@ import os
 
 @available(iOS 14.0, macOS 11, *)
 private struct Loggers {
-	static var loggers: [UUID: Logger] = [:]
+    static var loggers: [UUID : Logger] = [:]
 }
 
 struct L {
-	@inline(__always)
-	static let enabled: Bool = false
-
-	let subsystem: String
-	let category: String
-
-	private let shouldLog: Bool
-
-	private let id = UUID()
-
-	init(
-		subsystem: String = "com.nordicsemi.ios_ble_library", category: String,
-		enabled: Bool = Self.enabled
-	) {
-		self.subsystem = subsystem
-		self.category = category
-		self.shouldLog = enabled
-
-		if #available(iOS 14, macOS 11, *) {
-			Loggers.loggers[self.id] = Logger(subsystem: subsystem, category: category)
-		}
-	}
-
-	func i(_ msg: String) {
-		#if DEBUG
-			if !shouldLog { return }
-
-			if #available(iOS 14, macOS 11, *) {
-				Loggers.loggers[id]?.info("\(msg)")
-			} else {
-				os_log("%@", type: .info, msg)
-			}
-
-		#endif
-	}
-
-	func d(_ msg: String) {
-		#if DEBUG
-			if !shouldLog { return }
-			if #available(iOS 14, macOS 11, *) {
-				Loggers.loggers[id]?.debug("\(msg)")
-			} else {
-				os_log("%@", type: .debug, msg)
-			}
-		#endif
-	}
-
-	func e(_ msg: String) {
-		#if DEBUG
-			if !shouldLog { return }
-			if #available(iOS 14, macOS 11, *) {
-				Loggers.loggers[id]?.error("\(msg)")
-			} else {
-				os_log("%@", type: .error, msg)
-			}
-		#endif
-	}
-
-	func f(_ msg: String) {
-		#if DEBUG
-			if !shouldLog { return }
-			if #available(iOS 14, macOS 11, *) {
-				Loggers.loggers[id]?.fault("\(msg)")
-			} else {
-				os_log("%@", type: .fault, msg)
-			}
-		#endif
-	}
+    @inline(__always)
+    static let enabled: Bool = false
+    
+    let subsystem: String
+    let category: String
+    
+    private let shouldLog: Bool
+    
+    private let id = UUID()
+    
+    init(
+        subsystem: String = "com.nordicsemi.ios_ble_library", category: String,
+        enabled: Bool = Self.enabled
+    ) {
+        self.subsystem = subsystem
+        self.category = category
+        self.shouldLog = enabled
+        
+        if #available(iOS 14, macOS 11, *) {
+            Loggers.loggers[self.id] = Logger(subsystem: subsystem, category: category)
+        }
+    }
+    
+    func i(_ msg: String) {
+#if DEBUG
+        if !shouldLog { return }
+        
+        if #available(iOS 14, macOS 11, *) {
+            Loggers.loggers[id]?.info("\(msg)")
+        } else {
+            os_log("%@", type: .info, msg)
+        }
+        
+#endif
+    }
+    
+    func d(_ msg: String) {
+#if DEBUG
+        if !shouldLog { return }
+        if #available(iOS 14, macOS 11, *) {
+            Loggers.loggers[id]?.debug("\(msg)")
+        } else {
+            os_log("%@", type: .debug, msg)
+        }
+#endif
+    }
+    
+    func e(_ msg: String) {
+#if DEBUG
+        if !shouldLog { return }
+        if #available(iOS 14, macOS 11, *) {
+            Loggers.loggers[id]?.error("\(msg)")
+        } else {
+            os_log("%@", type: .error, msg)
+        }
+#endif
+    }
+    
+    func f(_ msg: String) {
+#if DEBUG
+        if !shouldLog { return }
+        if #available(iOS 14, macOS 11, *) {
+            Loggers.loggers[id]?.fault("\(msg)")
+        } else {
+            os_log("%@", type: .fault, msg)
+        }
+#endif
+    }
 }
