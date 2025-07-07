3af0a2026a9063968613b7d660f16ed237cf456e
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2025-07-07 10:05:56 +0100

    Revert "Copied files from native CoreBluetooth version to CoreBluetoothMock"
    This reverts commit d559c0a9ed637e8c52516e36d82c880234b1cb2a.

--- Sources/iOS-BLE-Library-Mock/Peripheral/Peripheral.swift
+++ Sources/iOS-BLE-Library-Mock/Peripheral/Peripheral.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  Peripheral.swift
 //
 //
 //  Created by Nick Kibysh on 28/04/2023.
@@ -10,9 +10,13 @@ import CoreBluetooth
 import CoreBluetoothMock
 import Foundation
 
+// MARK: - Observer
+
 private class Observer: NSObject {
 	func setup() {}
 }
+
+// MARK: - NativeObserver
 
 private class NativeObserver: Observer {
 	@objc private var peripheral: CoreBluetooth.CBPeripheral
@@ -40,6 +44,8 @@ private class NativeObserver: Observer {
 		}
 	}
 }
+
+// MARK: - MockObserver
 
 private class MockObserver: Observer {
 	@objc private var peripheral: CBMPeripheralMock
@@ -143,11 +149,11 @@ public class Peripheral {
 
 // MARK: - API
 
-extension Peripheral {
-
-	public func MTU() -> Int {
-		return peripheral.maximumWriteValueLength(for: .withoutResponse)
-	}
+public extension Peripheral {
+    
+    func MTU() -> Int {
+        return peripheral.maximumWriteValueLength(for: .withoutResponse)
+    }
 }
 
 // MARK: - Channels
@@ -289,27 +295,24 @@ extension Peripheral {
 		return characteristicReader.readValue(from: characteristic)
 	}
 
-	/// Listen for updates to the value of a characteristic.
-	///
-	/// - Parameter characteristic: The characteristic to monitor for updates.
-	/// - Returns: A publisher emitting characteristic values or an error.
-	public func listenValues(for characteristic: CBCharacteristic) -> AnyPublisher<Data, Error>
-	{
-		return peripheralDelegate.updatedCharacteristicValuesSubject
-			.filter {
-				let characteristicMatch = $0.0.uuid == characteristic.uuid
-				if let service = characteristic.service {
-					return characteristicMatch
-						&& service.uuid == $0.0.service?.uuid
-				} else {
-					return characteristicMatch
-				}
-			}
-			.tryCompactMap { (ch, err) in
-				if let err {
-					throw err
-				}
-
+    /// Listen for updates to the value of a characteristic.
+    ///
+    /// - Parameter characteristic: The characteristic to monitor for updates.
+    /// - Returns: A publisher emitting characteristic values or an error.
+    public func listenValues(for characteristic: CBCharacteristic) -> AnyPublisher<Data, Error> {
+        return peripheralDelegate.updatedCharacteristicValuesSubject
+            .filter {
+                let characteristicMatch = $0.0.uuid == characteristic.uuid
+                if let service = characteristic.service {
+                    return characteristicMatch && service.uuid == $0.0.service?.uuid
+                } else {
+                    return characteristicMatch
+                }
+            }
+            .tryCompactMap { (ch, err) in
+                if let err {
+                    throw err
+                }
 				return ch.value
 			}
 			.eraseToAnyPublisher()
@@ -325,6 +328,7 @@ extension Peripheral {
 }
 
 // MARK: - Writing Characteristic and Descriptor Values
+
 extension Peripheral {
 	/// Write data to a characteristic and wait for a response.
 	///
@@ -332,19 +336,16 @@ extension Peripheral {
 	///   - data: The data to write.
 	///   - characteristic: The characteristic to write to.
 	/// - Returns: A publisher indicating success or an error.
-	public func writeValueWithResponse(_ data: Data, for characteristic: CBCharacteristic)
-		-> AnyPublisher<Void, Error>
-	{
+	public func writeValueWithResponse(_ data: Data, for characteristic: CBCharacteristic) -> AnyPublisher<Void, Error> {
 		return peripheralDelegate.writtenCharacteristicValuesSubject
-			.first(where: {
-				let characteristicMatch = $0.0.uuid == characteristic.uuid
-				if let service = characteristic.service {
-					return characteristicMatch
-						&& service.uuid == $0.0.service?.uuid
-				} else {
-					return characteristicMatch
-				}
-			})
+            .first(where: {
+                let characteristicMatch = $0.0.uuid == characteristic.uuid
+                if let service = characteristic.service {
+                    return characteristicMatch && service.uuid == $0.0.service?.uuid
+                } else {
+                    return characteristicMatch
+                }
+            })
 			.tryMap { result in
 				if let e = result.1 {
 					throw e
@@ -377,9 +378,25 @@ extension Peripheral {
 	public func writeValue(_ data: Data, for descriptor: CBDescriptor) -> Future<Void, Error> {
 		return descriptorWriter.write(data, to: descriptor)
 	}
+    
+    public func isReadyToSendWriteWithoutResponse() -> AnyPublisher<Void, Never> {
+        isReadyToSendWriteWithoutResponseChannel
+            .bluetooth { [unowned self] in
+                guard self.peripheral.canSendWriteWithoutResponse else {
+                    // isReadyToSendWriteWithoutResponseSubject will fire on
+                    // peripheralIsReady() callback
+                    return
+                }
+                // Signal to continue.
+                self.peripheralDelegate.isReadyToSendWriteWithoutResponseSubject.send(Void())
+            }
+            .autoconnect()
+            .eraseToAnyPublisher()
+    }
 }
 
 // MARK: - Setting Notifications for a Characteristic’s Value
+
 extension Peripheral {
 	/// Set notification state for a characteristic.
 	///
@@ -387,9 +404,7 @@ extension Peripheral {
 	///   - isEnabled: Whether notifications should be enabled or disabled.
 	///   - characteristic: The characteristic for which to set the notification state.
 	/// - Returns: A publisher indicating success or an error.
-	public func setNotifyValue(_ isEnabled: Bool, for characteristic: CBCharacteristic)
-		-> AnyPublisher<Bool, Error>
-	{
+	public func setNotifyValue(_ isEnabled: Bool, for characteristic: CBCharacteristic) -> AnyPublisher<Bool, Error> {
 		if characteristic.isNotifying == isEnabled {
 			return Just(isEnabled)
 				.setFailureType(to: Error.self)
@@ -397,15 +412,14 @@ extension Peripheral {
 		}
 
 		return peripheralDelegate.notificationStateSubject
-			.first {
-				let characteristicMatch = $0.0.uuid == characteristic.uuid
-				if let service = characteristic.service {
-					return characteristicMatch
-						&& service.uuid == $0.0.service?.uuid
-				} else {
-					return characteristicMatch
-				}
-			}
+            .first {
+                let characteristicMatch = $0.0.uuid == characteristic.uuid
+                if let service = characteristic.service {
+                    return characteristicMatch && service.uuid == $0.0.service?.uuid
+                } else {
+                    return characteristicMatch
+                }
+            }
 			.tryMap { result in
 				if let error = result.1 {
 					throw error
@@ -421,8 +435,10 @@ extension Peripheral {
 }
 
 // MARK: - Accessing a Peripheral’s Signal Strengthin page link
+
 extension Peripheral {
-	/// Retrieves the current RSSI value for the peripheral while connected to the central manager.
+	
+    /// Retrieves the current RSSI value for the peripheral while connected to the central manager.
 	public func readRSSI() -> AnyPublisher<NSNumber, Error> {
 		peripheralDelegate.readRSSISubject
 			.tryMap { rssi in
--- Sources/iOS-BLE-Library-Mock/Peripheral/ReactivePeripheralDelegate.swift
+++ Sources/iOS-BLE-Library-Mock/Peripheral/ReactivePeripheralDelegate.swift
@@ -132,6 +132,15 @@ open class ReactivePeripheralDelegate: NSObject, CBPeripheralDelegate {
 		discoveredServicesQueue.runNext()
 	}
 
+	/*
+	public func peripheral(
+		_ peripheral: CBPeripheral, didDiscoverIncludedServicesFor service: CBService,
+		error: Error?
+	) {
+		discoveredIncludedServicesSubject.send((service, service.includedServices, error))
+	}
+     */
+
 	// MARK: Discovering Characteristics and their Descriptors
 
 	open func peripheral(
