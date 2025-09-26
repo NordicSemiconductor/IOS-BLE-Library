f6d8477d6c0b1ab8c648e531596013a43280ea6d
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2025-09-26 11:19:37 +0100

    Improved MTU() - now maximumWriteValueLength() API
    The name is now in line with what the function returns. And we've also improved its attached documentation by a lot.

---
+++ Sources/iOS-BLE-Library-Mock/Peripheral/PeripheralError.swift
@@ -0,0 +1,25 @@
+//
+//  PeripheralError.swift
+//  iOS-BLE-Library
+//
+//  Created by Dinesh Harjani on 26/9/25.
+//  Copyright © 2025 Nordic Semiconductor ASA. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - PeripheralError
+
+public enum PeripheralError: LocalizedError {
+    
+    case onlyConnectedPeripheralsHaveNegotiatedMTU
+    
+    // MARK: Description
+    
+    public var errorDescription: String? {
+        switch self {
+        case .onlyConnectedPeripheralsHaveNegotiatedMTU:
+            return "A connected Peripheral is required to obtain a valid negotiated MTU (Maximum Transmission Unit) size."
+        }
+    }
+}
